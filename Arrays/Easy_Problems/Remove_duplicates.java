62de6ba1e4b242b7178c713428102d2c85fbf60c
Prakash Peram <122421617+PERAMPRAKASH@users.noreply.github.com>
2025-07-02 06:28:37 +0530

    Add files via upload

---
+++ Arrays/Easy_Problems/Remove_duplicates.java
@@ -0,0 +1,17 @@
+class Remove_duplicates{
+    public static int Remove_duplicates(int[] arr){
+        int n = arr.length;
+        int i=0;
+        for(int j=1;j<n;j++){
+            if(arr[i]!=arr[j]){
+                i++;
+                arr[i]=arr[j];
+            }
+        }
+        return i+1;
+    }
+    public static void main(String[] args){
+        int[] arr={1,1,2,2,3,3,4};
+        System.out.println(Remove_duplicates(arr));
+    }
+}
