be2f55f4a39a15a691a9506fe955f84e46c0d537
PERAMPRAKASH <prakashperam143@gmail.com>
2025-07-31 07:10:00 +0530

    committing changes

--- Arrays/Easy_Problems/Sorted_Array_or_Not.java
+++ Arrays/Easy_Problems/Sorted_Array_or_Not.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Sorted_Array{
 	public static boolean Sorted_Array(int[] arr){
 	for(int i=1;i<arr.length;i++){
@@ -12,19 +11,4 @@ class Sorted_Array{
 		int[] arr={5,7,6,2,8};
 		System.out.println(Sorted_Array(arr));
 	}
-=======
-class Sorted_Array{
-	public static boolean Sorted_Array(int[] arr){
-	for(int i=1;i<arr.length;i++){
-		if(arr[i]<arr[i-1]){
-			return false;
-		}
-	}
-	return true;
-	}
-	public static void main(String[] args){
-		int[] arr={5,7,6,2,8};
-		System.out.println(Sorted_Array(arr));
-	}
->>>>>>> 01932a1bcaf3754455b4e26695ebe2a55cfae90d
 }
